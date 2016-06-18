9e0726ab1b0f8a44f6f8d441834907ac95a971ee
Aniket PRabhu <iamthemananiket@gmail.com>
2016-06-18 13:14:20 -0400

    first commit

---
+++ codeEditor/index.php
@@ -0,0 +1,130 @@
+<?php
+ob_start();
+?>
+<pre>
+<?php
+function parse($text) {
+    // Damn pesky carriage returns...
+    $text = str_replace("\r\n", "\n", $text);
+    $text = str_replace("\r", "\n", $text);
+
+    // JSON requires new line characters be escaped
+    $text = str_replace("\n", "\\n", $text);
+    return $text;
+}
+ // header("Content-Type: text/plain");
+function get_project_data($project){
+	// print_r($project);
+	$project_contents = array();
+	foreach ($project as $key => $value) {
+		for ($i=0; $i < count($value); $i++) {
+			$file = $project[$key][$i];
+			$file_and_data = array(); 
+			if(file_exists($file)){
+				$fd = fopen($file, "r");
+				$content = "";
+				while($s=fread($fd, 100)){
+					 // echo $s;
+					$content = $content.$s;
+				}
+				$file_and_data[$file] = $content;
+				$project[$key][$i] = $file_and_data;
+			}
+		}
+	}
+	// print_r($project);
+	// echo json_encode($project_contents);
+	echo (json_encode($project)) ;
+}
+?>
+<?php
+	if(isset($_GET["project"]))
+	{
+		$project = $_GET["project"];
+		//echo $project;
+		if(is_dir($project)){
+			$path = $project;
+			$project_files = array('js' =>  "", 'html' => "", 'css' =>  "");
+			$path_list = array($path);
+			while(count($path_list) && count($files = scandir($path_list[count($path_list) - 1])) > 2){
+				// print_r($files);
+				$parent_dir = $path_list[count($path_list) - 1];
+				array_pop($path_list);
+				foreach ($files as $key => $value) {
+					if($value != '.' and $value != '..'){
+						// echo($value."\n");
+						if(@ereg(".+\.js", $value))	{
+							$project_files["js"] = $project_files["js"].$parent_dir.'/'.$value.'&';
+						}
+						if(@ereg(".+\.css", $value))	{
+							$project_files["css"] = $project_files["css"].$parent_dir.'/'.$value.'&';
+						}
+						if(@ereg(".+\.html", $value))	{
+							$project_files["html"] = $project_files["html"].$parent_dir.'/'.$value.'&';
+						}
+						if(is_dir($parent_dir.'/'.$value))
+							array_push($path_list, $parent_dir.'/'.$value);												
+					}
+				}
+				// echo "Path List";
+				 // print_r($path_list);
+			}
+			$project_value = "";
+			// print_r($project_files);
+			foreach ($project_files as $key => $value) {
+				// echo $key.":".$value;
+				$value = substr($value, 0, strlen($value) - 1);
+				$value_array = explode('&', $value);
+				for ($i=0; $i < count($value_array); $i++) { 
+					$project_value = $project_value.$key.'[]='.$value_array[$i].'&';
+				}
+					
+			}
+			$project_value = substr($project_value, 0, strlen($project_value) - 1);
+			// echo $project_value.'\n';
+			parse_str($project_value, $temp);
+			// print_r($temp);
+			if(setcookie('project', $project_value, 0, '/'))
+				;// ;echo "cookie set";
+			else
+				echo "Not set";
+			// echo $temp;
+			get_project_data($temp);
+
+		}else{
+			echo "Invalid Path".$project;
+		}
+	}
+	if(isset($_GET['code'])){
+		parse_str($_COOKIE['project'], $temp);
+		// print_r($temp);
+		$content = json_decode(parse($_GET['code']), true);
+		// var_dump($temp);
+		// print_r($content);
+		foreach ($content as $i => $j) {
+			foreach ($j as $k => $l) {
+				foreach ($l as $key => $value) {
+					$dirs = explode('/', $key);
+					$path = "";
+					if(count($dirs) > 1){
+						$path = implode('/', array_slice($dirs, 0, $length = count($dirs)- 1));
+						echo $path;
+						echo "<><><><<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>";
+
+						if(!is_dir($path) && !file_exists($key))
+							mkdir ($path , $mode = 0777 , $recursive = true );
+					}
+					$fd = fopen($key, "w");
+					fwrite($fd, $value);
+					fclose($fd);
+					# code...
+				}
+				# code...
+			}
+
+		}
+
+	}
+	ob_flush();
+?>
+</pre>
---
+++ js/code_editor.js
@@ -0,0 +1,264 @@
+// js.initHighlightingOnLoad();
+var project = {}
+var projectName = ""
+var cause = "input";
+var httpRequest;
+var editors = {}
+var count = 1;
+function setCookie(fileType, className) {
+	var cookie = decodeURIComponent(document.cookie);
+	var i = cookie.search(fileType+"=");
+	console.log(i, fileType, className, cookie)
+	if(i >= 0){
+		cookie = cookie.replace(fileType+"[]=", fileType+"[]="+projectName+"/"+className+"&")
+	}else
+		cookie+= "&"+fileType+"[]="+projectName+"/"+className;
+	console.log(cookie)
+	document.cookie = cookie
+}
+function updateContent(event) {
+	project[code.getAttribute("type")][code.getAttribute("j")][code.className] = code.value;	
+	code.innerHTML = code.value;
+	editors[code.className] = code;
+	output.contentWindow.clientHeight = output.clientHeight;
+	output.contentWindow.clientWidth = output.clientWidth;
+	output.setAttribute("srcdoc", editors[projectName+"/"+"index.html"].value);
+
+}
+function createButtonAndItsEditor(fileType,j) {
+	className = (Object.keys(project[fileType][j])[0]);
+	var editor = document.createElement('textarea');
+	editor.id = "code";
+	editor.setAttribute("type", fileType);
+	editor.setAttribute("j", j);
+	editor.className = className;
+	editor.value = project[fileType][j][className];
+	editor.addEventListener("input", updateContent);
+	editor.addEventListener("keydown", function (event) {
+		if (event.ctrlKey){
+			if(event.which == 'M'.charCodeAt(0))
+				saveFile()
+		}
+		
+	});			
+	editor.innerHTML = project[fileType][j][className];
+	editors[className] = editor;
+	var button = document.createElement('button');
+	button.innerHTML = className.replace(projectName+"/", "");
+	button.id = className;
+	button.className = "btn btn-info editorButtons";
+	button.className = "btn btn-info btn-sm editorButtons";	
+	button.addEventListener("click",function(event) {
+		code.remove();
+		codeArea.insertBefore(editors[event.target.id], codeArea.firstChild);
+		output.contentWindow.clientHeight = output.clientHeight;
+		output.contentWindow.clientWidth = output.clientWidth;
+		output.setAttribute("srcdoc", editors[projectName+"/"+"index.html"].value);
+	});
+	if(!document.getElementById(className))
+		buttonArea.insertBefore(button, newFileBox);
+		// codeArea.appendChild(button);	
+}
+
+function buildCodeArea() {
+	var button = document.createElement('button');
+	var fileBox = document.createElement('input');
+	//fileBox.hidden = true;
+	fileBox.id = "newFileBox";
+	fileBox.className = "newFileBox";
+	fileBox.size = 4;
+	button.id = "newFile";
+	button.className = "btn btn-warning newButtons";
+
+	button.textContent = "+"
+
+	if (!document.getElementById(button.id)) {
+		buttonArea.appendChild(button);
+		buttonArea.insertBefore(fileBox, button);
+	}
+
+	for(var i in project){
+		for(var j in project[i]){
+			console.log(i, j)
+			createButtonAndItsEditor(i, j)
+		}
+	}
+	if (count % 2 != 0) {
+		fileBox.hidden = true;
+		
+		$(document).ready(function () {
+			$('#newFile').hover(function () {
+				fileBox.hidden = false;
+			});
+			$('#newFile').click(function () {
+				var className = $('.newFileBox').val();
+				
+				if (className.length != 0) {
+					var tmp = /\.[a-z]+$/i;
+					
+					var fileType = tmp.exec(className)[0].split(".")[1];
+					console.log(className,fileType);
+					console.log("Add UI to insert name and destination directory ")
+					console.log(project[fileType]);
+					project[fileType].push({[projectName+"/"+className]:""});
+					console.log(project)
+					console.log(editors)
+					setCookie(fileType, className)
+					createButtonAndItsEditor(fileType, project[fileType].length - 1);					
+								
+				}
+			});
+
+		});
+	}
+	console.log(button.id);
+	count++;
+	console.log(editors)
+	if(window["code"])	
+		code.remove();
+	codeArea.insertBefore(editors[projectName+"/"+"index.html"], codeArea.firstChild);	
+	output.contentWindow.clientHeight = output.clientHeight;
+	output.contentWindow.clientWidth = output.clientWidth;
+	output.setAttribute("srcdoc", editors[projectName+"/"+"index.html"].value);
+}
+function addClassCodeEditor() {
+	var i = 0
+	var stack = [output.contentWindow.document]
+	while (stack.length > 0) {
+		var element = stack.pop();
+		var elementChildren = element.children;
+		for (var e = 0; e < elementChildren.length; e++) {
+			elementChildren[e].className = ((elementChildren[e].className)?(elementChildren[e].className+" "):"") +"codeEditor";
+			// elementChildren[e].className += "codeEditor"+i;
+			stack.push(elementChildren[e]);
+		}
+	}
+
+}
+function replaceScriptAndCssTags() {
+	console.log(editors)
+	var scripts = output.contentWindow.document.getElementsByTagName('script');
+	// console.log(scripts)
+	for(var script = 0; script < scripts.length; script++){
+		if(scripts[script].src != ""){
+			var foundReplacement = false;
+			s = output.contentWindow.document.createElement('script');
+			s.type = "text/javascript";
+			console.log(editors, scripts[script].src)
+			for(var i in editors){
+
+				if(scripts[script].src.search(i.replace(projectName+"/", ""))!= -1){
+					console.log(editors[i].innerText)
+					s.text = editors[i].innerText;
+					foundReplacement = true;
+				}
+			}
+			if(foundReplacement){
+				scripts[script].parentNode.replaceChild(s, scripts[script])
+				script--;
+			}
+		}
+	}
+	var csss = output.contentWindow.document.getElementsByTagName('link');
+	// console.log(output.contentWindow.document)
+	// console.log(csss, csss.length)
+	for(var css = 0; css < csss.length; css++){
+		// console.log(css, csss[css].href, csss.length)
+		if(csss[css].href != ""){
+			s = output.contentWindow.document.createElement('style');
+			var foundReplacement = false;
+			for(var i in editors){
+				console.log("inside", csss[css].href, i)
+				if(csss[css].href.search(i.replace(projectName+"/", ""))!= -1){
+					s.innerHTML =  editors[i].value;
+					foundReplacement = true;
+					break;
+				}
+			}			
+
+			if(foundReplacement){
+				csss[css].parentNode.replaceChild(s, csss[css])
+				css--;
+			}
+		}
+		// console.log(css, csss[css].href, csss.length)
+	}
+}
+
+function displayDOM(element, dom_window){
+	dom_window.document.body.appendChild(element.cloneNode(true))
+	for(var i = 0; i < element.children.length; i++){
+		displayDOM(element.children[i], dom_window)
+	}
+	
+}
+function getSourceCode(event) {
+	if (event.ctrlKey){
+		element = event.target
+		dom_window = window.open('', '_blank', 'height=200,width=200,modal=yes,alwaysRaised=yes')
+		displayDOM(element, dom_window)
+		event.stopPropagation();
+	}
+}
+function saveFile() {
+	httpRequest = new XMLHttpRequest();
+	console.log(project)
+	urlParametes = "code="+encodeURIComponent(JSON.stringify(project));
+
+	// urlParametes = urlParametes.replace("'", "\'")	
+	// console.log(urlParametes)
+	if(document.cookie.search(projectName) >= 0){
+	     httpRequest.open("get", "./codeEditor?"+urlParametes, true);
+	     httpRequest.onreadystatechange = function() {
+	     	console.log(httpRequest.responseText)
+	     	// body...
+	     }
+	     httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	     httpRequest.send();
+	}	
+}
+function getResponseCode(event) {
+	if(httpRequest.readyState = XMLHttpRequest.DONE)
+		if(httpRequest.status == 200){
+			var response = httpRequest.responseText
+			console.log(response);
+			// console.log(response.slice(5, response.length-6))
+			project = JSON.parse(response.slice(5, response.length-7))
+			console.log(project)
+			buildCodeArea();
+
+		}
+}
+
+code.addEventListener("input", updateContent);
+code.addEventListener("keydown", function (event) {
+	if (event.ctrlKey){
+		// console.log("Fine", event.which, 'm'.charCodeAt(0))
+		if(event.which == 'M'.charCodeAt(0))
+			saveFile()
+	}
+	
+});
+output.addEventListener("load", function (event) {
+	addClassCodeEditor();
+	var allELements = output.contentWindow.document.getElementsByClassName("codeEditor");
+	for (var e = 0; e < allELements.length; e++) {
+		allELements[e].removeEventListener("click", getSourceCode)
+		allELements[e].addEventListener("click", getSourceCode, true)
+	}
+	output.contentWindow.document.body.style.zoom = output.contentWindow.innerWidth / window.document.body.clientWidth;	
+	replaceScriptAndCssTags();
+})
+go.addEventListener("click", function checkValidityOfPath(event) {
+	httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = getResponseCode;
+	projectName = server_location.value;
+    httpRequest.open("get", "./codeEditor?project="+server_location.value, true);
+    // console.log(server_location.value);
+    httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    httpRequest.send();		
+})
+$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
